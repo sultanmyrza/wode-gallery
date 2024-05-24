2dd49f17c2361fdf61fb6c9cafb15923dc0c211d
sultanmyrza <sultanmyrza@gmail.com>
2024-05-24 14:39:07 +0800

    uploadthing added

--- README.md
+++ README.md
@@ -8,8 +8,9 @@
 - [x] Actually set up a database (vercel postgres)
 - [x] Attach databask to UI
 - [x] Add authentication (w/ clerk)
-- [ ] Add image upload
+- [x] Add image upload
 - [ ] "taint" (server-only)
+- [ ] Use Next/Image component
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
 import TopNav from "@/app/_components/topnav";
 import { ClerkProvider } from "@clerk/nextjs";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { GeistSans } from "geist/font/sans";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata = {
   title: "WoDe Gallery App",
@@ -18,6 +22,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
